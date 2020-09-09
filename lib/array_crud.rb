914e1d8c313923c9608ef07076c0626a26729613
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-09 22:47:35 +0000

    Automatically backed up by Learn

---
+++ lib/array_crud.rb
@@ -0,0 +1,11 @@
+def create_an_empty_array
+  array = []
+end
+
+def create_an_array
+  array = ["bill", "dale", "boomhower", "hank"]
+end
+
+def add_element_to_end_of_array(array)
+  array.push
+end
