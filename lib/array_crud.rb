fd97b9646976dae188c8a0eb41e9366e309b1755
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-09 22:49:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,6 +6,14 @@ def create_an_array
   array = ["bill", "dale", "boomhower", "hank"]
 end
 
-def add_element_to_end_of_array(array)
-  array.push
+def add_element_to_end_of_array(array, element)
+  array.push(element)
+end
+
+def add_element_to_start_of_array(array, element)
+  array.unshift(element)
+end
+
+def remove_element_from_end_of_array(array)
+  array.pop
 end
